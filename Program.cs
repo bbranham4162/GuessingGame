2ba01145c5f7d26606fae31ef25ad0fbb3921f2f
Bryson B <brysonbranham@gmail.com>
2022-04-28 14:32:16 -0600

    FinishedGuessingGame

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Does this work???");
+using System;
+
+namespace CheckPoint 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+        Console.WriteLine("What was your score? ");
+
+        int score = Int32.Parse(Console.ReadLine());
+
+        string letterGrade ;
+        if (score >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (score >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (score >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (score >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.Write($"You got a {letterGrade} in the class");
+        }
+    }
+}
